8790f3be13de2512827cad1866dc31c32c7f908d
Duncan Calvert <dcalvert@ihmc.us>
2020-10-07 12:16:29 -0500

    Convert build files to KTS.

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+   id("us.ihmc.ihmc-build") version "0.22.0"
+   id("us.ihmc.ihmc-ci") version "5.0"
+   id("us.ihmc.ihmc-cd") version "1.7"
+}
+
+ihmc {
+   group = "us.ihmc"
+   version = "0.12.5"
+   vcsUrl = "https://github.com/ihmcrobotics/ihmc-geometry"
+   openSource = true
+
+   configureDependencyResolution()
+   configurePublications()
+}
+
+mainDependencies {
+   api("us.ihmc:euclid-geometry:0.12.1")
+   api("org.ejml:core:0.30")
+   api("org.ejml:dense64:0.30")
+   api("us.ihmc:ihmc-commons:0.26.6")
+   api("net.sf.trove4j:trove4j:3.0.3")
+}
+
+testDependencies {
+   api("us.ihmc:ihmc-commons-testing:0.26.6")
+}
